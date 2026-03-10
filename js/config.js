9989ec9c7535db4c0fc72b8aa5c4d911d96f3e63
JBlehrmühlhuber <jonas.blehrmuehlhuber@gmx.at>
2026-03-10 14:52:32 +0100

    Config changes

--- js/config.js
+++ js/config.js
@@ -1,5 +1,9 @@
-export const CONFIG = Object.freeze({
-    CARDS_SEQUENCE: [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1],
-    TOTAL_ROUNDS: 13,
-    POINTS_BASE: 5
-});
+export const CONFIG = (() => {
+    const CARDS_SEQUENCE = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1];
+
+    return Object.freeze({
+        CARDS_SEQUENCE,
+        TOTAL_ROUNDS: CARDS_SEQUENCE.length,
+        POINTS_BASE: 5
+    });
+})();
